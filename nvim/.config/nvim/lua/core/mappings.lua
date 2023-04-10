ef50ab859968a318b50c09bf4905b44fd0c603f6
Catgroove <martin.norberg@storytel.com>
2023-04-10 14:52:21 +0200

    telescope and tmux config update

--- nvim/.config/nvim/lua/core/mappings.lua
+++ nvim/.config/nvim/lua/core/mappings.lua
@@ -3,7 +3,7 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex) 
 
 -- Telescope
-vim.keymap.set("n", "<leader>ff", "<cmd> Telescope find_files <CR>")
+vim.keymap.set("n", "<leader>ff", "<cmd> Telescope find_files hidden=true <CR>")
 vim.keymap.set("n", "<leader>fo", "<cmd> Telescope oldfiles <CR>")
 vim.keymap.set("n", "<leader>fw", "<cmd> Telescope live_grep <CR>")
 
--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -3,6 +3,14 @@ local default_plugins = {
 		"nvim-telescope/telescope.nvim",
 		tag = "0.1.1",
 		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = function()
+			return require "plugins.configs.telescope"
+		end,
+
+		config = function(_, opts)
+			local telescope = require "telescope"
+			telescope.setup(opts)
+		end,
 	},
 	{
 		"catppuccin/nvim",
