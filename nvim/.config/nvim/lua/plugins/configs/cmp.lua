7c99444766fccb78dd67ea1d1695ef6bf2c908de
Catgroove <martin.norberg@storytel.com>
2023-04-13 20:18:28 +0200

    removed unused comments

--- nvim/.config/nvim/lua/plugins/configs/cmp.lua
+++ nvim/.config/nvim/lua/plugins/configs/cmp.lua
@@ -1,21 +1,8 @@
 local cmp = require("cmp")
 
 return {
-
 	completion = {
 		completeopt = "menu,menuone,noinsert",
-	},
-
-	window = {
-		-- completion = {
-		--   side_padding = (cmp_style ~= "atom" and cmp_style ~= "atom_colored") and 1 or 0,
-		--   winhighlight = "Normal:CmpPmenu,CursorLine:CmpSel,Search:PmenuSel",
-		--   scrollbar = false,
-		-- },
-		-- documentation = {
-		-- 	border = border("CmpDocBorder"),
-		-- 	winhighlight = "Normal:CmpDoc",
-		-- },
 	},
 	snippet = {
 		expand = function(args)
@@ -44,30 +31,6 @@ return {
 			behavior = cmp.ConfirmBehavior.Replace,
 			select = false,
 		}),
-		-- ["<Tab>"] = cmp.mapping(function(fallback)
-		-- 	if cmp.visible() then
-		-- 		cmp.select_next_item()
-		-- 	elseif require("luasnip").expand_or_jumpable() then
-		-- 		vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
-		-- 	else
-		-- 		fallback()
-		-- 	end
-		-- end, {
-		-- 	"i",
-		-- 	"s",
-		-- }),
-		-- ["<S-Tab>"] = cmp.mapping(function(fallback)
-		-- 	if cmp.visible() then
-		-- 		cmp.select_prev_item()
-		-- 	elseif require("luasnip").jumpable(-1) then
-		-- 		vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
-		-- 	else
-		-- 		fallback()
-		-- 	end
-		-- end, {
-		-- 	"i",
-		-- 	"s",
-		-- }),
 	},
 	sources = {
 		{ name = "nvim_lsp" },
