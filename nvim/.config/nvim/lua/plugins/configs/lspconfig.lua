8e2dd149273266776549ef2d3041237b3e84c2ce
Catgroove <martin.norberg@storytel.com>
2023-04-14 20:40:23 +0200

    dont run stupid code action

--- nvim/.config/nvim/lua/plugins/configs/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/configs/lspconfig.lua
@@ -5,7 +5,7 @@ local on_attach = function(_, bufnr)
 	keymap.set("n", "gd", vim.lsp.buf.definition, opts)
 	keymap.set("n", "gr", "<cmd>Telescope lsp_references<CR>")
 	keymap.set("n", "K", vim.lsp.buf.hover, opts)
-	keymap.set("n", "vca", vim.lsp.buf.code_action(), opts)
+	keymap.set("n", "vca", vim.lsp.buf.code_action, opts)
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
