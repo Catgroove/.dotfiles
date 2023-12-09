8210ef137e9318c08fe80b115e68dd0b2f0f8e89
Martin Norberg <martinnorberg@A00174.local>
2023-12-09 23:46:39 +0100

    highlight on yank (experiment)

--- nvim/.config/nvim/lua/martinnorberg/core/autocmds.lua
+++ nvim/.config/nvim/lua/martinnorberg/core/autocmds.lua
@@ -1,7 +1,16 @@
+-- open telescope on startup
 vim.api.nvim_create_autocmd("VimEnter", {
 	callback = function()
 		if vim.fn.argv(0) == "" then
 			require("telescope.builtin").find_files({ hidden = true })
 		end
+	end,
+})
+
+-- highlight on yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+	group = vim.api.nvim_create_augroup("highlight_yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
 	end,
 })
