ef8e31adee70ebf695d7b8a3455168d4cefe6095
Martin Norberg <martinnorberg@A00174.local>
2023-12-09 22:08:35 +0100

    Replace -> Insert for nvim-cmp for confirm action

--- nvim/.config/nvim/lua/martinnorberg/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/martinnorberg/plugins/nvim-cmp.lua
@@ -68,15 +68,15 @@ return {
 			},
 
 			mapping = {
-				["<C-k>"] = cmp.mapping.select_prev_item(),
-				["<C-j>"] = cmp.mapping.select_next_item(),
+				["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+				["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
 				["<C-d>"] = cmp.mapping.scroll_docs(-4),
 				["<C-f>"] = cmp.mapping.scroll_docs(4),
 				["<C-Space>"] = cmp.mapping.complete(),
 				["<C-e>"] = cmp.mapping.close(),
 				["<CR>"] = cmp.mapping.confirm({
-					behavior = cmp.ConfirmBehavior.Replace,
-					select = false,
+					behavior = cmp.ConfirmBehavior.Insert,
+					select = true,
 				}),
 			},
 			sources = {
