f6c85934d840d2f7fe73e33a07d9dc39c3380d29
Martin Norberg <martinnorberg@A00174.local>
2023-12-16 11:17:25 +0100

    add catppuccin theme to mason

--- nvim/.config/nvim/lua/martinnorberg/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/martinnorberg/plugins/catppuccin.lua
@@ -2,6 +2,12 @@ return {
 	"catppuccin/nvim",
 	name = "catppuccin",
 	config = function()
+		require("catppuccin").setup({
+			integrations = {
+				mason = true,
+			},
+		})
+
 		vim.cmd.colorscheme("catppuccin")
 	end,
 }
