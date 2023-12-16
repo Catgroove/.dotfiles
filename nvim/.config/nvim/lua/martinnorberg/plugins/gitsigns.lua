d50ac281a073c502f756084d198a181d72431760
Martin Norberg <nobbehhh@gmail.com>
2023-12-16 20:42:00 +0100

    git blame via gitsigns

--- nvim/.config/nvim/lua/martinnorberg/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/martinnorberg/plugins/gitsigns.lua
@@ -10,5 +10,16 @@ return {
 			changedelete = { text = "▎" },
 			untracked = { text = "▎" },
 		},
+		on_attach = function(buffer)
+			local gs = package.loaded.gitsigns
+
+			local function map(mode, l, r, desc)
+				vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
+			end
+
+			map("n", "<leader>gb", function()
+				gs.blame_line({ full = true })
+			end, "Blame Line")
+		end,
 	},
 }
