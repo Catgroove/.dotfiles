ea01bfc5ef433e3dfdb3d400f9b9bdb080ae0e49
Martin Norberg <nobbehhh@gmail.com>
2024-01-07 14:26:09 +0100

    incremental selection treesitter

--- nvim/.config/nvim/lua/martinnorberg/plugins/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/martinnorberg/plugins/nvim-treesitter.lua
@@ -30,6 +30,16 @@ return {
 			},
 
 			indent = { enable = true },
+
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<c-space>",
+					node_incremental = "<c-space>",
+					scope_incremental = "<c-s>",
+					node_decremental = "<M-space>",
+				},
+			},
 		})
 	end,
 }
