b2d3a29bd77a5ec818b675050407cf4350f03f59
Martin Norberg <nobbehhh@gmail.com>
2024-01-07 14:13:58 +0100

    lsp

--- nvim/.config/nvim/lua/martinnorberg/plugins/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/martinnorberg/plugins/nvim-lspconfig.lua
@@ -25,6 +25,7 @@ return {
 		end
 
 		local capabilities = vim.lsp.protocol.make_client_capabilities()
+		capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
 		local lspconfig = require("lspconfig")
 		local util = require("lspconfig/util")
