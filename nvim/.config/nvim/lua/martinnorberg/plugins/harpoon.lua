a65af8f1638586c7f416e6b5764e45123b723c12
Martin Norberg <nobbehhh@gmail.com>
2024-01-17 10:00:44 +0100

    go back to explicit file navigation for harpoon

--- nvim/.config/nvim/lua/martinnorberg/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/martinnorberg/plugins/harpoon.lua
@@ -7,10 +7,10 @@ return {
 		vim.keymap.set("n", "<leader>a", mark.add_file)
 		vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 		vim.keymap.set("n", "<C-s>", function()
-			ui.nav_prev()
+			ui.nav_file(1)
 		end)
 		vim.keymap.set("n", "<C-n>", function()
-			ui.nav_next()
+			ui.nav_file(2)
 		end)
 
 		require("harpoon").setup({})
