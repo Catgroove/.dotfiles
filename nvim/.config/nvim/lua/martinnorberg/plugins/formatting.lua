e81a9d05ad31e89b2f2e54bafdd20713a0bba94d
Martin Norberg <nobbehhh@gmail.com>
2024-03-03 14:23:53 +0100

    complete formatting and lsp setup

--- nvim/.config/nvim/lua/martinnorberg/plugins/formatting.lua
+++ nvim/.config/nvim/lua/martinnorberg/plugins/formatting.lua
@@ -1,14 +1,33 @@
 return {
 	"stevearc/conform.nvim",
+	event = { "BufWritePre" },
 	opts = {
-		notify_on_error = false,
 		format_on_save = {
 			timeout_ms = 500,
 			lsp_fallback = true,
 		},
-
 		formatters_by_ft = {
+			-- lua
 			lua = { "stylua" },
+
+			-- go
+			go = { "gofumpt", "goimports", "goimportstidy" },
+
+			-- js/ts
+			typescript = { "prettierd" },
+			typescriptreact = { "prettierd" },
+			javascript = { "prettierd" },
+			javascriptreact = { "prettierd" },
+			json = { "prettierd" },
+			html = { "prettierd" },
+			css = { "prettierd" },
+		},
+		formatters = {
+			goimportstidy = {
+				command = "goimportstidy",
+				args = { "-w", "-local", "github.com/shipwallet", "$FILENAME" },
+				stdin = false,
+			},
 		},
 	},
 }
