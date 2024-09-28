5ffe9968a8cecdcf41791c67d5a70569216f2595
Martin Norberg <nobbehhh@gmail.com>
2024-09-28 22:11:32 +0200

    load grug far properly

--- nvim/.config/nvim/lua/martinnorberg/plugins/editor.lua
+++ nvim/.config/nvim/lua/martinnorberg/plugins/editor.lua
@@ -111,11 +111,13 @@ return {
 	},
 
 	-- Search and Replace
-	"MagicDuck/grug-far.nvim",
-	config = function()
-		local grug = require("grug-far")
-		grug.setup({})
+	{
+		"MagicDuck/grug-far.nvim",
+		config = function()
+			local grug = require("grug-far")
+			grug.setup({})
 
-		vim.keymap.set("n", "<leader>S", grug.open, { desc = "[S]earch and Replace" })
-	end,
+			vim.keymap.set("n", "<leader>S", grug.open, { desc = "[S]earch and Replace" })
+		end,
+	},
 }
