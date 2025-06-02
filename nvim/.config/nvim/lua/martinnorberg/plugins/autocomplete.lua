6f4f2268e9c14b91b7dea52ebecdc0d6b40704e8
Martin Norberg <nobbehhh@gmail.com>
2025-06-02 10:20:01 +0200

    migrate to blink

--- nvim/.config/nvim/lua/martinnorberg/plugins/autocomplete.lua
+++ nvim/.config/nvim/lua/martinnorberg/plugins/autocomplete.lua
@@ -1,55 +1,29 @@
 return {
-	"hrsh7th/nvim-cmp",
-	event = "InsertEnter",
-	dependencies = {
-		-- Snippet engine
-		"L3MON4D3/LuaSnip",
-		"saadparwaiz1/cmp_luasnip",
-
-		-- Icons
-		"onsails/lspkind.nvim",
-
-		-- LSP completion capabilities
-		"hrsh7th/cmp-nvim-lsp",
-		"hrsh7th/cmp-path",
-		"hrsh7th/cmp-buffer",
+	"saghen/blink.cmp",
+	version = "*",
+	---@module 'blink.cmp'
+	---@type blink.cmp.Config
+	opts = {
+		keymap = {
+			["<CR>"] = { "accept", "fallback" },
+			["<C-k>"] = { "select_prev", "fallback" },
+			["<C-j>"] = { "select_next", "fallback" },
+		},
+		completion = {
+			documentation = {
+				auto_show = true,
+			},
+			menu = {
+				draw = {
+					treesitter = { "lsp" },
+				},
+			},
+		},
+		sources = {
+			default = { "lsp", "path", "snippets", "buffer" },
+		},
 	},
-	config = function()
-		local cmp = require("cmp")
-		local luasnip = require("luasnip")
-		local lspkind = require("lspkind")
-
-		luasnip.config.setup({})
-
-		cmp.setup({
-			snippet = {
-				expand = function(args)
-					luasnip.lsp_expand(args.body)
-				end,
-			},
-
-			completion = { completeopt = "menu,menuone,noinsert" },
-
-			formatting = {
-				format = lspkind.cmp_format({
-					mode = "symbol_text",
-					maxwidth = 50,
-					ellipsis_char = "...",
-				}),
-			},
-
-			mapping = cmp.mapping.preset.insert({
-				["<C-j>"] = cmp.mapping.select_next_item(),
-				["<C-k>"] = cmp.mapping.select_prev_item(),
-				["<CR>"] = cmp.mapping.confirm({ select = true }),
-			}),
-
-			sources = {
-				{ name = "nvim_lsp" },
-				{ name = "luasnip" },
-				{ name = "path" },
-				{ name = "buffer" },
-			},
-		})
-	end,
+	opts_extend = {
+		"sources.default",
+	},
 }
