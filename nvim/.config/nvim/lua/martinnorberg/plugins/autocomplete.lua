a5b4f0dac4e78d28aefb6eaa1183336878809871
Martin Norberg <nobbehhh@gmail.com>
2025-06-02 10:21:59 +0200

    add appearance config to blink

--- nvim/.config/nvim/lua/martinnorberg/plugins/autocomplete.lua
+++ nvim/.config/nvim/lua/martinnorberg/plugins/autocomplete.lua
@@ -9,6 +9,9 @@ return {
 			["<C-k>"] = { "select_prev", "fallback" },
 			["<C-j>"] = { "select_next", "fallback" },
 		},
+		appearance = {
+			nerd_font_variant = "mono",
+		},
 		completion = {
 			documentation = {
 				auto_show = true,
