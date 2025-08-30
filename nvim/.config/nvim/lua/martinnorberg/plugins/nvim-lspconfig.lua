47224229649fb525d4c27df61e24a2afc40ad32b
Martin Norberg <nobbehhh@gmail.com>
2025-08-30 10:32:54 +0200

    install lazydev

--- nvim/.config/nvim/lua/martinnorberg/plugins/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/martinnorberg/plugins/nvim-lspconfig.lua
@@ -38,13 +38,6 @@ return {
 					settings = {
 						Lua = {
 							runtime = { version = "LuaJIT" },
-							workspace = {
-								checkThirdParty = false,
-								library = {
-									"${3rd}/luv/library",
-									unpack(vim.api.nvim_get_runtime_file("", true)),
-								},
-							},
 							completion = {
 								callSnippet = "Replace",
 							},
