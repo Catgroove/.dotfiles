25b41f2d2aaf0968546c2356fe0f18ed631f944a
Martin Norberg <nobbehhh@gmail.com>
2025-11-29 11:36:56 +0100

    orbstack in fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -37,3 +37,7 @@ abbr sec "secret"
 abbr t "debug_trace"
 
 abbr vim "nvim"
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
